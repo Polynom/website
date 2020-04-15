ab2077cb20bda3a76b8e467d5252592ed2093cce
x <jelacic.josef@gmail.com>
2020-04-15 17:00:28 +0200

    typo fix

--- src/store/modules/support.js
+++ src/store/modules/support.js
@@ -13,8 +13,7 @@ const state = {
     item2: {
       logo: '',
       name: 'Paralelni Polis',
-      description: 'Unique artistic concept consisting of a "cryptocurrency-only" cafe & coworking space, event venue and R42$@Ra' +
-          'hackerspace.',
+      description: 'Unique artistic concept consisting of a "cryptocurrency-only" cafe & coworking space, event venue and hackarspace',
       link: 'https://www.paralelnipolis.cz/o-nas/en/'
     }
   }
